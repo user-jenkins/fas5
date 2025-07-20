251e156589958fcd8f178e0ed6de1005085d3a7f
user-jenkins <79544295+user-jenkins@users.noreply.github.com>
2025-07-20 17:19:33 -0400

    Add/update tf/main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
   region = "us-west-2"
 }
-resource "aws_eks_cluster" "fas5" {
+resource "aws_eks_cluster" "fas5-eks" {
   name     = "fas5-eks-cluster"
-  role_arn = aws_iam_role.fas5.arn
+  role_arn = aws_iam_role.fas5-eks.arn
+  depends_on = [aws_iam_role_policy_attachment.fas5-eks-AmazonEKSClusterPolicy]
 }
